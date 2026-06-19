0f3059c351f515e82822b34f51e233718e505e08
Sany khan <momin879@gmail.om>
2026-06-19 11:47:11 +0600

    Separate upload URL from asset URL

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => env('PUBLIC_FILESYSTEM_ROOT', storage_path('app/public')),
-            'url' => env('ASSET_URL', '/storage'),
+            'url' => env('PUBLIC_FILESYSTEM_URL', rtrim(env('APP_URL', ''), '/') . '/storage'),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
