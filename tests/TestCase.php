a8fbab6e5eaf3af92ee31c7a0274a9ed981f9e55
Sany khan <momin879@gmail.om>
2026-06-19 11:50:32 +0600

    Isolate tests from local database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication(): Application
+    {
+        $app = parent::createApplication();
+
+        $app['config']->set([
+            'app.env' => 'testing',
+            'database.default' => 'sqlite',
+            'database.connections.sqlite.database' => ':memory:',
+            'cache.default' => 'array',
+            'session.driver' => 'array',
+            'queue.default' => 'sync',
+        ]);
+        $app['db']->purge();
+
+        return $app;
+    }
 }
