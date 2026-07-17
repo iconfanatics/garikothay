bb4b4035c2c3615534b478529e3fb069a0c76cb1
Sany khan <momin879@gmail.om>
2026-07-18 01:00:36 +0600

    Fix vendor slip rendering issue completely

--- resources/views/pdf/vendor-slip.blade.php
+++ resources/views/pdf/vendor-slip.blade.php
@@ -44,7 +44,11 @@
                 <td style="width: 50%; text-align: right;">
                     <strong>Order #:</strong> {{ $order->order_number }}<br>
                     <strong>Date:</strong> {{ $order->created_at->format('M d, Y') }}<br>
-                    <strong>Payment Method:</strong> <span class="status-badge">{{ strtoupper($order->payment_method instanceof \App\Enums\PaymentMethod ? $order->payment_method->label() : ($order->payment_method ?? 'N/A')) }}</span>
+                    @php
+                        $pm = $order->payment_method;
+                        $pmStr = $pm instanceof \App\Enums\PaymentMethod ? $pm->label() : (is_string($pm) ? $pm : 'N/A');
+                    @endphp
+                    <strong>Payment Method:</strong> <span class="status-badge">{{ strtoupper($pmStr) }}</span>
                 </td>
             </tr>
         </table>
