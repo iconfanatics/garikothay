be429aba926a9861ac1aec98525ac9ebf52cc9a0
Sany khan <momin879@gmail.om>
2026-08-17 10:07:09 +0600

    Fix TypeError in NavigationItemResource by adding missing Builder import

--- app/Filament/Resources/NavigationItemResource.php
+++ app/Filament/Resources/NavigationItemResource.php
@@ -9,6 +9,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class NavigationItemResource extends Resource
 {
