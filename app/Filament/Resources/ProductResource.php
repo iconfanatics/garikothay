de4a48c1be9e8d8a976afd7b7c6f3f7c66b34680
Sany khan <momin879@gmail.om>
2026-08-20 10:31:54 +0600

    Fix VariantsRelationManager namespace in ProductResource

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -1001,7 +1001,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            RelationManagers\VariantsRelationManager::class,
+            ProductResource\RelationManagers\VariantsRelationManager::class,
         ];
     }
 
